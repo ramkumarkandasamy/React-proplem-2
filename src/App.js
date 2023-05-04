c640f343d8a66cdea44d53550fd3eb02b57ca223
ramkumarkandasamy <ramkumarkandasamy@gmail.com>
2023-05-04 14:22:15 +0000

    Still facing some problem

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './Components/UserProfile/index'
+
+const userDetails = {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'praneetha',
+    role: 'Software Engineer'
+}
+
+const App = () => <UserProfile />
+
+export default App
